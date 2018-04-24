b58af0dd041d23c0817b4439f13986fd7428e440
sinjie11 <sinjiezzang@hanmail.net>
2018-04-24 15:59:12 +0900

    Initial commit

---
+++ app/src/main/java/edu/android/airquality_api/BasicData_All.java
@@ -0,0 +1,286 @@
+package edu.android.airquality_api;
+
+public class BasicData_All {
+
+    private int resultCode;         // 결과 코드
+    private String resultMsg;       // 결과 메시지
+    private int numOfRows;          // 한 페이지 결과수
+    private int pageNo;             // 페이지 번호
+    private int totalCount;         // 전체 결과 수
+    private String stationName;     // 측정소 명
+    private String mangName;        // 측정망
+    private String umdName;         // 주소(읍면동 명)
+    private String dataTime;        // 측정일
+    private double so2Value;        // 아황산 가스 농도 (단위 ppm)
+    private double coValue;         // 일산화 탄소 농도 (단위 ppm)
+    private double o3Value;         // 오존 농도 (단위 ppm)
+    private double no2Value;        // 이산화 질소 농도 (단위 ppm)
+    private double pm10Value;       // 미세먼지 농도 (단위 ㎍/㎥)
+    private double pm10Value24;     // 미세먼지 24시간 예측 농도 (단위 : ㎍/㎥)
+    private double pm25Value;       // 미세먼지 농도 (PM2.5) (단위 : ㎍/㎥)
+    private double pm25Value24;     // 미세먼지 PM25 24 시간 예측 이동 농도 (단위 : ㎍/㎥)
+    private double khaiValue;       // 통합대기환경 수치
+    private double khaiGrade;       // 통합대기환경 지수
+    private double so2Grade;        // 아황산 가스 지수
+    private double coGrade;         // 일산화 탄소 지수
+    private double o3Grade;         // 오존 지수
+    private double no2Grade;        // 이산화 질소 지수
+    private double pm10Grade;       // 미세먼지 PM10 24시간 등급
+    private double pm25Grade;       // 미세먼지 PM2.5 24시간 등급
+    private double pm10Grade1h;     // 미세먼지 PM10 1시간 등급
+    private double pm25Grade1h;     // 미세먼지 PM2.5 1시간 등급
+
+    /**
+     * @ getter / setter
+     */
+
+    public int getResultCode() {
+        return resultCode;
+    }
+
+    public void setResultCode(int resultCode) {
+        this.resultCode = resultCode;
+    }
+
+    public String getResultMsg() {
+        return resultMsg;
+    }
+
+    public void setResultMsg(String resultMsg) {
+        this.resultMsg = resultMsg;
+    }
+
+    public int getNumOfRows() {
+        return numOfRows;
+    }
+
+    public void setNumOfRows(int numOfRows) {
+        this.numOfRows = numOfRows;
+    }
+
+    public int getPageNo() {
+        return pageNo;
+    }
+
+    public void setPageNo(int pageNo) {
+        this.pageNo = pageNo;
+    }
+
+    public int getTotalCount() {
+        return totalCount;
+    }
+
+    public void setTotalCount(int totalCount) {
+        this.totalCount = totalCount;
+    }
+
+    public String getStationName() {
+        return stationName;
+    }
+
+    public void setStationName(String stationName) {
+        this.stationName = stationName;
+    }
+
+    public String getMangName() {
+        return mangName;
+    }
+
+    public void setMangName(String mangName) {
+        this.mangName = mangName;
+    }
+
+    public String getUmdName() {
+        return umdName;
+    }
+
+    public void setUmdName(String umdName) {
+        this.umdName = umdName;
+    }
+
+    public String getDataTime() {
+        return dataTime;
+    }
+
+    public void setDataTime(String dataTime) {
+        this.dataTime = dataTime;
+    }
+
+    public double getSo2Value() {
+        return so2Value;
+    }
+
+    public void setSo2Value(double so2Value) {
+        this.so2Value = so2Value;
+    }
+
+    public double getCoValue() {
+        return coValue;
+    }
+
+    public void setCoValue(double coValue) {
+        this.coValue = coValue;
+    }
+
+    public double getO3Value() {
+        return o3Value;
+    }
+
+    public void setO3Value(double o3Value) {
+        this.o3Value = o3Value;
+    }
+
+    public double getNo2Value() {
+        return no2Value;
+    }
+
+    public void setNo2Value(double no2Value) {
+        this.no2Value = no2Value;
+    }
+
+    public double getPm10Value() {
+        return pm10Value;
+    }
+
+    public void setPm10Value(double pm10Value) {
+        this.pm10Value = pm10Value;
+    }
+
+    public double getPm10Value24() {
+        return pm10Value24;
+    }
+
+    public void setPm10Value24(double pm10Value24) {
+        this.pm10Value24 = pm10Value24;
+    }
+
+    public double getPm25Value() {
+        return pm25Value;
+    }
+
+    public void setPm25Value(double pm25Value) {
+        this.pm25Value = pm25Value;
+    }
+
+    public double getPm25Value24() {
+        return pm25Value24;
+    }
+
+    public void setPm25Value24(double pm25Value24) {
+        this.pm25Value24 = pm25Value24;
+    }
+
+    public double getKhaiValue() {
+        return khaiValue;
+    }
+
+    public void setKhaiValue(double khaiValue) {
+        this.khaiValue = khaiValue;
+    }
+
+    public double getKhaiGrade() {
+        return khaiGrade;
+    }
+
+    public void setKhaiGrade(double khaiGrade) {
+        this.khaiGrade = khaiGrade;
+    }
+
+    public double getSo2Grade() {
+        return so2Grade;
+    }
+
+    public void setSo2Grade(double so2Grade) {
+        this.so2Grade = so2Grade;
+    }
+
+    public double getCoGrade() {
+        return coGrade;
+    }
+
+    public void setCoGrade(double coGrade) {
+        this.coGrade = coGrade;
+    }
+
+    public double getO3Grade() {
+        return o3Grade;
+    }
+
+    public void setO3Grade(double o3Grade) {
+        this.o3Grade = o3Grade;
+    }
+
+    public double getNo2Grade() {
+        return no2Grade;
+    }
+
+    public void setNo2Grade(double no2Grade) {
+        this.no2Grade = no2Grade;
+    }
+
+    public double getPm10Grade() {
+        return pm10Grade;
+    }
+
+    public void setPm10Grade(double pm10Grade) {
+        this.pm10Grade = pm10Grade;
+    }
+
+    public double getPm25Grade() {
+        return pm25Grade;
+    }
+
+    public void setPm25Grade(double pm25Grade) {
+        this.pm25Grade = pm25Grade;
+    }
+
+    public double getPm10Grade1h() {
+        return pm10Grade1h;
+    }
+
+    public void setPm10Grade1h(double pm10Grade1h) {
+        this.pm10Grade1h = pm10Grade1h;
+    }
+
+    public double getPm25Grade1h() {
+        return pm25Grade1h;
+    }
+
+    public void setPm25Grade1h(double pm25Grade1h) {
+        this.pm25Grade1h = pm25Grade1h;
+    }
+
+    @Override
+    public String toString() {
+        return "BasicData_All{" +
+                "resultCode=" + resultCode +
+                ", resultMsg='" + resultMsg + '\'' +
+                ", numOfRows=" + numOfRows +
+                ", pageNo=" + pageNo +
+                ", totalCount=" + totalCount +
+                ", stationName='" + stationName + '\'' +
+                ", mangName='" + mangName + '\'' +
+                ", umdName='" + umdName + '\'' +
+                ", dataTime='" + dataTime + '\'' +
+                ", so2Value=" + so2Value +
+                ", coValue=" + coValue +
+                ", o3Value=" + o3Value +
+                ", no2Value=" + no2Value +
+                ", pm10Value=" + pm10Value +
+                ", pm10Value24=" + pm10Value24 +
+                ", pm25Value=" + pm25Value +
+                ", pm25Value24=" + pm25Value24 +
+                ", khaiValue=" + khaiValue +
+                ", khaiGrade=" + khaiGrade +
+                ", so2Grade=" + so2Grade +
+                ", coGrade=" + coGrade +
+                ", o3Grade=" + o3Grade +
+                ", no2Grade=" + no2Grade +
+                ", pm10Grade=" + pm10Grade +
+                ", pm25Grade=" + pm25Grade +
+                ", pm10Grade1h=" + pm10Grade1h +
+                ", pm25Grade1h=" + pm25Grade1h +
+                '}';
+    }
+
+} // end class BasicData_All
---
+++ app/src/main/java/edu/android/airquality_api/MainActivity.java
@@ -0,0 +1,204 @@
+package edu.android.airquality_api;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    /**
+     * ※ 대기오염정보 조회 Open Api 주소 (개발, 운영 서버 같음)
+     * - OpenApi 서비스명: ArpltnInforInqireSvc
+     * - 실시간 정보 : 10분 (매 시간 시간자료 갱신은 20분 전후로 반영됨)
+     * - 대기질 예보 정보 : 매 시간 22분, 57분
+     */
+
+    /**
+     * ※ 서울특별시 지역구 리스트 (25개)
+     **/
+    public static final String[] seoul = {
+            "종로구", "중구", "용산구", "성동구", "광진구",
+            "동대문구", "중랑구", "성북구", "강북구", "도봉구",
+            "노원구", "은평구", "서대문구", "마포구", "양천구",
+            "강서구", "구로구", "금천구", "영등포구", "동작구",
+            "관악구", "서초구", "강남구", "송파구", "강동구"};
+
+
+    /**
+     * Grade 값 의미
+     * <p>
+     * 1 - 좋음
+     * 2 - 보통
+     * 3 - 나쁨
+     * 4 - 매우나쁨
+     */
+
+    private TextView textDataTime, textMang, textName, textKhaiValue, textKhaiGrade, textPm10Value, textPm25Value;
+    private EditText editText;
+
+    ArrayList<BasicData_All> basicData_alls;
+
+    private String tag;
+
+    private String inputText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textDataTime = findViewById(R.id.textDataTime);
+        textMang = findViewById(R.id.textMang);
+        textName = findViewById(R.id.textName);
+        textKhaiValue = findViewById(R.id.textKhaiValue);
+        textKhaiGrade = findViewById(R.id.textKhaiGrade);
+        textPm10Value = findViewById(R.id.textPm10Value);
+        textPm25Value = findViewById(R.id.textPm25Value);
+
+        editText = findViewById(R.id.editText);
+
+    } // end onCreate
+
+
+    public void addSearch(View view) {
+
+        new GetXmlDataAll().execute();
+
+    } // end addSearch
+
+    public ArrayList<BasicData_All> listBasicData_Alls() {
+
+        ArrayList<BasicData_All> lists = new ArrayList<>();
+
+        inputText = editText.getText().toString();
+
+        // 주소 검색 ( 구, 지방의 경우 동으로 되어있음)
+        String stationUrl =
+                "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?stationName="
+                        + inputText
+                        + "&dataTerm=month&pageNo=1&numOfRows=1&ServiceKey=2WjM1G6ETI%2F3HKoHrAC9MhjgY3PufrijH35VWAgVnh3A5ZrEkBkXovDVizsiQoKm7FDHO2AmW4LG%2FA2oiF8new%3D%3D&ver=1.3";
+
+        try {
+            Thread.sleep(500);
+            URL url = new URL(stationUrl);
+            InputStream is = url.openStream();
+
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser xpp = factory.newPullParser();
+            xpp.setInput(new InputStreamReader(is));
+            String tag;
+            xpp.next();
+
+            int eventType = xpp.getEventType();
+            BasicData_All basicData_all = new BasicData_All();
+
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                switch (eventType) {
+                    case XmlPullParser.START_DOCUMENT: // 파싱시작
+                        break;
+
+                    case XmlPullParser.START_TAG:
+                        tag = xpp.getName(); // 태그이름 얻어오기
+
+                        if (tag.equals("item")) {
+
+                        } else if (tag.equals("dataTime")) { // 측정일시
+                            xpp.next();
+                            basicData_all.setDataTime(xpp.getText());
+
+                        } else if (tag.equals("mangName")) { // 측정망 정보
+                            xpp.next();
+                            basicData_all.setMangName(xpp.getText());
+
+                        } else if (tag.equals("pm10Value")) { // 미세먼지 (단위:㎍/㎥)
+                            xpp.next();
+                            basicData_all.setPm10Value(Double.parseDouble(xpp.getText()));
+
+                        } else if (tag.equals("pm25Value")) { // 초미세먼지 (단위:㎍/㎥)
+                            xpp.next();
+                            basicData_all.setPm25Value(Double.parseDouble(xpp.getText()));
+
+                        } else if (tag.equals("KhaiValue")) { // 통합대기환경 수치
+                            xpp.next();
+                            basicData_all.setKhaiValue(Double.parseDouble(xpp.getText()));
+
+                        } else if (tag.equals("KhaiGrade")) { // 통합대기환경 등급
+                            xpp.next();
+                            basicData_all.setKhaiGrade(Double.parseDouble(xpp.getText()));
+
+                        }
+                        break;
+                    case XmlPullParser.TEXT:
+                        break;
+
+                    case XmlPullParser.END_TAG:
+                        tag = xpp.getName(); // 태그 이름 얻어오기
+                        if (tag.equals("item")) {
+                            lists.add(basicData_all);
+                            basicData_all = new BasicData_All();
+                        }
+
+                        break;
+                }
+
+                eventType = xpp.next();
+            } // end while
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        } // end try-catch
+
+        return lists;
+
+    } // end ArrayList<BasicData_All>
+
+    class GetXmlDataAll extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            basicData_alls = listBasicData_Alls();
+            return null;
+        }
+
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            textDataTime.setText("날짜 및 시간 : ");
+            textMang.setText("측정망 정보 : ");
+            textName.setText("측정소 명: ");
+            textKhaiValue.setText("통합대기환경 지수 : ");
+            textKhaiGrade.setText("통합대기환경 등급 : ");
+            textPm10Value.setText("미세먼지 : ");
+            textPm25Value.setText("초미세먼지 : ");
+
+            for (int i = 0; i < basicData_alls.size(); i++) {
+
+                textDataTime.append(basicData_alls.get(i).getDataTime());
+                textMang.append(basicData_alls.get(i).getMangName());
+                textName.append(inputText);
+                textKhaiValue.append(basicData_alls.get(i).getKhaiValue() + " ");
+                textKhaiGrade.append(basicData_alls.get(i).getKhaiGrade() + " ");
+                textPm10Value.append(basicData_alls.get(i).getPm10Value() + " " + "㎍/㎥");
+                textPm25Value.append(basicData_alls.get(i).getPm25Value() + " " + "㎍/㎥");
+
+            } // end for
+
+        } // end onPostExecute
+
+    } // end GetXmlDataAll
+
+} // end class MainActivity
